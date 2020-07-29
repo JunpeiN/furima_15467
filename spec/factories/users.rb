47cbb118543875187220962ff04b0c81199ca754
JunpeiN <junpei10191019@gmail.com>
2020-07-29 23:08:31 +0900

    ユーザー管理機能テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"aiueo1"}
     password_confirmation {password}
+    family_name           {"山田"}
+    first_name            {"太朗"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"1990-10-19"}
   end
 end
