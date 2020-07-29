1e8bb8905a73acae0618d1ac74aa16f25743835c
JunpeiN <junpei10191019@gmail.com>
2020-07-29 12:37:21 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users,controllers: {
+    sessions: 'users/sessions'
+  }
   get 'items/index'
-  devise_for :users
   #仮のルーティング設定
   root "items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
