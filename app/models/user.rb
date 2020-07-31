e2fc618d31b058ea0e84d4bd8b8f0723d3b63d11
JunpeiN <junpei10191019@gmail.com>
2020-07-31 11:47:33 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-  validates :password, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/ }
+  zkaku = /\A[ぁ-んァ-ン一-龥]/
+  zkana = /\A[ァ-ヶー－]+\z/
+  eisu = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: { with: zkaku } do
+      validates :family_name
+      validates :first_name
+    end
+
+    with_options format: { with: zkana } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
+  validates :password, confirmation: true, format: { with: eisu }
 end
