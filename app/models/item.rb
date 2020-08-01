425c0467b7ef25e8e99985bde393ce6a88da1e77
JunpeiN <junpei10191019@gmail.com>
2020-08-01 12:19:55 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :status
   belongs_to_active_hash :until_shipping
-  belong_to :user
+  belongs_to :user
 
   with_options presence: true do
     validates :name
     validates :img 
-    validates :price, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :description
     validates :category
     validates :delivery_cost
