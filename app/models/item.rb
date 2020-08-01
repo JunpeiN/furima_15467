2ca5f218861730ae0d5cb855fb0c4febca2fe372
JunpeiN <junpei10191019@gmail.com>
2020-08-01 11:47:28 +0900

    モデル作成

---
+++ app/models/item.rb
@@ -0,0 +1,28 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :delivery_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :status
+  belongs_to_active_hash :until_shipping
+
+  with_options presence: true do
+    validates :name
+    validates :img 
+    validates :price, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999
+    validates :description
+    validates :category
+    validates :delivery_cost
+    validates :prefecture
+    validates :status
+    validates :until_shipping
+  end
+  
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :status_id
+    validates :until_shipping_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_050630) do
+ActiveRecord::Schema.define(version: 2020_08_01_012556) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "img", null: false
+    t.integer "price", null: false
+    t.integer "delivery_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "until_shipping_id", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.text "description", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_07_28_050630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
