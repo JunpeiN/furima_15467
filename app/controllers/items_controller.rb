2ef674cf8b2e4f79bd91198174702abcb7a097f3
JunpeiN <junpei10191019@gmail.com>
2020-08-01 19:48:02 +0900

    ルーティング、コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:price,:img,:description,:delivery_cost_id,:prefecture_id,:until_shipping_id,:category_id,:status_id,:user_id)
+  end
+
 end
