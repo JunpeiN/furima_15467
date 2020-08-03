86fc9bba0c5e1b6d3cf84e3e4c5c6d4910608201
JunpeiN <junpei10191019@gmail.com>
2020-08-03 10:35:56 +0900

    単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name            { Faker::Name.initials(number: 2) }
+    price           { 300 }
+    delivery_cost_id   { 2 }
+    until_shipping_id  { 2 }
+    prefecture_id      { 2 }
+    category_id        { 2 }
+    status_id          { 2 }
+    description { '説明' }
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      user = FactoryBot.create(:user)
+      @item = FactoryBot.build(:item, user: user)
+      @item.image = fixture_file_upload('public/images/test_image.png')
+    end
+
+    it '全て条件通り入力されていれば登録できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'priceが空では登録できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが300円未満では登録できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+    end
+
+    it 'priceが9,999,999より高いと登録できないこと' do
+      @item.price = 10_000_000
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+    end
+
+    it 'descriptionが空では登録できないこと' do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'prefectureを選択しないと登録できないこと' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+    end
+
+    it 'categoryを選択しないと登録できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
+    end
+
+    it 'statusを選択しないと登録できないこと' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Status must be other than 1')
+    end
+
+    it 'unti]_shippingを選択しないと登録できないこと' do
+      @item.until_shipping_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Until shipping must be other than 1')
+    end
+
+    it 'delivery_costを選択しないと登録できないこと' do
+      @item.delivery_cost_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Delivery cost must be other than 1')
+    end
+
+    it 'imageが空では登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+  end
 end
