21213e6eec766505adf816c08fea01cdd244704b
JunpeiN <junpei10191019@gmail.com>
2020-08-06 19:10:52 +0900

    複数テーブルへの保存の設定

---
+++ app/forms/item_address.rb
@@ -0,0 +1,20 @@
+class ItemAddress
+
+  include ActiveModel::Model
+  attr_accessor :home_number, :item_id,:user_id,:postal_code,:prefecture_id,:city,:building_name,:phone_number
+
+  with_options presence: true do
+    validates :prefecture_id ,numericality: { other_than: 1 }
+    validates :city 
+    validates :home_number
+    validates :phone_number
+    validates :postal_code,format: { with: /\A\d{3}[-]\d{4}\z/}
+  end
+
+  def save
+    # 住所の情報を保存
+    Address.create(home_number: home_number,postal_code: postal_code, prefecture_id: prefecture_id, city: city, phone_number: phone_number, building_name: building_name,item_id: item_id)
+    # 購入商品の情報を保存
+    Buyer.create(item_id: item_id, user_id: user_id)
+  end
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,11 +3,5 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :item
 
-  with_options presence: true do
-    validates :prefecture_id ,numericality: { other_than: 1 }
-    validates :city 
-    validates :address
-    validates :phone_number
-    validates :postal_code
-  end
+  
 end
