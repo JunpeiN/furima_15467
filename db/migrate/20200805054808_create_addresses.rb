4811aea6fb904ef7e517042e5e52eebbd7c87e1c
JunpeiN <junpei10191019@gmail.com>
2020-08-06 19:09:00 +0900

    カラム修正

--- db/migrate/20200805054808_create_addresses.rb
+++ db/migrate/20200805054808_create_addresses.rb
@@ -4,7 +4,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :postal_code   ,null: false 
       t.integer    :prefecture_id ,null: false 
       t.string     :city          ,null: false 
-      t.string     :address       ,null: false 
+      t.string     :home_number   ,null: false 
       t.string     :building_name          
       t.string     :phone_number  ,null: false 
       t.references :item          ,null: false, foreign_key: true 
